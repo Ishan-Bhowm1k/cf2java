64f52a197ae0cc935b1a93ee09147cb319b0f310
Ishan-Bhowm1k <148938844+Ishan-Bhowm1k@users.noreply.github.com>
2023-10-25 11:12:16 +0530

    Add files via upload

---
+++ cfToJava/src/main/java/com/bounteous/cfToJava/CfToJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bounteous.cfToJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfToJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfToJavaApplication.class, args);
+	}
+
+}
